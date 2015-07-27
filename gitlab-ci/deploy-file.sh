8ae7933a9fc5c3c632401face2d1dd908a69527e
Patrizio Bekerle <office@bekerle.com>
2015-07-27 08:30:42 +0200

    finished deployment process

--- gitlab-ci/deploy-file.sh
+++ gitlab-ci/deploy-file.sh
@@ -5,4 +5,4 @@
 # execute: ./deploy-file.sh $RELEASE_ID $GITHUB_ACCESS_TOKEN
 #
 
-curl -H "Authorization: token $2" -H "Accept: application/vnd.github.manifold-preview" -H "Content-Type: application/x-apple-diskimage" --data-binary @QOwnNotes.dmg "https://uploads.github.com/repos/pbek/testrepo/releases/$1/assets?name=QOwnNotes.dmg"
+curl -H "Authorization: token $2" -H "Accept: application/vnd.github.manifold-preview" -H "Content-Type: application/x-apple-diskimage" --data-binary @QOwnNotes.dmg "https://uploads.github.com/repos/pbek/QOwnNotes/releases/$1/assets?name=QOwnNotes.dmg"
