c44459edee8ba2d58ce3299fa922e934d775c9d9
Patrizio Bekerle <office@bekerle.com>
2016-01-01 11:59:48 +0100

    more building stuff

--- ubuntu-launchpad/build-for-launchpad.sh
+++ ubuntu-launchpad/build-for-launchpad.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # This is the build script for Ubuntu Launchpad
 # https://launchpad.net/~pbek/+archive/ubuntu/qownnotes
@@ -7,14 +7,16 @@
 # sudo apt-get install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder cdbs
 #
 # The GPG public key $GPG_PUBLIC_KEY also has to be in place locally
+# Also a ~/.dput.cf has to be in place
 #
 
 # uncomment this if you want to force a version
-#QOWNNOTES_VERSION=0.67.5
+#QOWNNOTES_VERSION=0.68.6
 
 BRANCH=develop
 #BRANCH=master
-DISTRO=wily
+UBUNTU_RELEASES=( "vivid" "wily" "xenial" )
+
 
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
 PROJECT_PATH="/tmp/QOwnNotes-$$"
@@ -22,7 +24,6 @@ CUR_DIR=$(pwd)
 UPLOAD="true"
 DEBUILD_ARGS=""
 GPG_PUBLIC_KEY=D55B7124
-CHANGELOG_TEXT="Released $QOWNNOTES_VERSION"
 export DEBFULLNAME="Patrizio Bekerle"
 export DEBEMAIL="patrizio@bekerle.com"
 
@@ -57,37 +58,55 @@ else
     echo "#define VERSION \"$QOWNNOTES_VERSION\"" > src/version.h
 fi
 
-QOWNNOTES_SRC_DIR="qownnotes_$QOWNNOTES_VERSION"
-
-# rename and archive the src directory
-mv src $QOWNNOTES_SRC_DIR
-tar -czf $QOWNNOTES_SRC_DIR.orig.tar.gz $QOWNNOTES_SRC_DIR
-
-cd $QOWNNOTES_SRC_DIR
+changelogText="Released $QOWNNOTES_VERSION"
 
 echo "Using version $QOWNNOTES_VERSION..."
 
-# update the changelog file
-dch -v $QOWNNOTES_VERSION-1ubuntu1 $CHANGELOG_TEXT
-dch -r $CHANGELOG_TEXT
+# build for every Ubuntu release
+for ubuntuRelease in "${UBUNTU_RELEASES[@]}"
+do
+    :
+    echo "Building for $ubuntuRelease..."
 
-#Create the changelog file
-#echo "qownnotes ($QOWNNOTES_VERSION-1ubuntu1) $DISTRO; urgency=low" > debian/changelog
-#echo "" >> debian/changelog
-#echo "  * Released $QOWNNOTES_VERSION" >> debian/changelog
-#echo "" >> debian/changelog
-#echo " -- $DEBFULLNAME <$DEBEMAIL>  $DATE" >> debian/changelog
+    versionPart="$QOWNNOTES_VERSION-1ubuntu3ppa1~${ubuntuRelease}1"
+    qownnotesSrcDir="qownnotes_${QOWNNOTES_VERSION}"
 
-# launch debuild
-debuild -S -sa -k$GPG_PUBLIC_KEY $DEBUILD_ARGS
-cd ..
+    # copy the src directory
+    cp -R src $qownnotesSrcDir
 
-# send to launchpad
-if [ "$UPLOAD" = "true" ]; then
-    dput ppa:pbek/qownnotes $QOWNNOTES_SRC_DIR-1ubuntu1_source.changes
-fi;
+    # set release string to disable the update check
+    echo "#define RELEASE \"Ubuntu Linux ${ubuntuRelease^}\"" > $qownnotesSrcDir/release.h
+
+    # archive the source code
+    tar -czf $qownnotesSrcDir.orig.tar.gz $qownnotesSrcDir
+
+    cd $qownnotesSrcDir
+
+    # update the changelog file
+    #dch -v $versionPart $changelogText
+    #dch -r $changelogText
+    
+    changelogPath=debian/changelog
+
+    # create the changelog file
+    echo "qownnotes ($versionPart) $ubuntuRelease; urgency=low" > $changelogPath
+    echo "" >> $changelogPath
+    echo "  * $changelogText" >> $changelogPath
+    echo "" >> $changelogPath
+    echo " -- $DEBFULLNAME <$DEBEMAIL>  $DATE" >> $changelogPath
+
+    # launch debuild
+    debuild -S -sa -k$GPG_PUBLIC_KEY $DEBUILD_ARGS
+    cd ..
+
+    # send to launchpad
+    if [ "$UPLOAD" = "true" ]; then
+        dput ppa:pbek/qownnotes qownnotes_${versionPart}_source.changes
+    fi;
+done
+
 
 # remove everything after we are done
-if [ -d $PROJECT_PATH ]; then
-    rm -rf $PROJECT_PATH
-fi
+#if [ -d $PROJECT_PATH ]; then
+#    rm -rf $PROJECT_PATH
+#fi
