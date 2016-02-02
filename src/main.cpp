300691e598b1944372d04f5a2067c964854d6ad1
Patrizio Bekerle <office@bekerle.com>
2016-02-02 08:12:01 +0100

    loading more translation files

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,50 @@ int main(int argc, char *argv[])
     QCoreApplication::addLibraryPath("./");
 #endif
 
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
 
     QString locale = QLocale::system().name().section('_', 0, 0);
+    qDebug() << __func__ << " - 'locale': " << locale;
 
-    QTranslator translator;
-    QString path;
-
-#ifdef QT_DEBUG
-    path = "../src/languages";
-#else
-    path = "/usr/share/QOwnNotes/languages";
+#ifndef QT_DEBUG
+    QTranslator translatorRelease;
+    translatorRelease.load("/usr/share/QOwnNotes/languages/QOwnNotes_" + locale);
+    app.installTranslator(&translatorRelease);
 #endif
 
-    translator.load(path + "/QOwnNotes_" + locale);
-    a.installTranslator(&translator);
+    QTranslator translator1;
+    translator1.load(QCoreApplication::applicationDirPath() +
+                             "/../src/languages/QOwnNotes_" + locale);
+
+    QTranslator translator2;
+    translator2.load(QCoreApplication::applicationDirPath() +
+                             "/../languages/QOwnNotes_" + locale);
+    app.installTranslator(&translator2);
+
+    QTranslator translator3;
+    translator3.load(QCoreApplication::applicationDirPath() +
+                             "/languages/QOwnNotes_" + locale);
+    app.installTranslator(&translator3);
+
+    QTranslator translator4;
+    translator4.load(QCoreApplication::applicationDirPath() +
+                             "/QOwnNotes_" + locale);
+    app.installTranslator(&translator4);
+
+    QTranslator translator5;
+    translator5.load("../src/languages/QOwnNotes_" + locale);
+    app.installTranslator(&translator5);
+
+    QTranslator translatorLocal;
+    translatorLocal.load("QOwnNotes_" + locale);
+    app.installTranslator(&translatorLocal);
 
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
