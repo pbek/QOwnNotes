f1b236142e5ed483c0c5be00b7f172aae20e227d
Patrizio Bekerle <office@bekerle.com>
2016-02-14 08:37:40 +0100

    updated qm files

--- src/languages/QOwnNotes_de.ts
+++ src/languages/QOwnNotes_de.ts
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.0" language="de_DE">
+<defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>AboutDialog</name>
     <message>
