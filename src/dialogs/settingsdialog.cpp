2814b8bbb7f1ebeb31d984589334b1272fd668ff
Patrizio Bekerle <office@bekerle.com>
2016-04-19 18:23:20 +0200

    do an other resize trick

--- src/dialogs/settingsdialog.cpp
+++ src/dialogs/settingsdialog.cpp
@@ -22,12 +22,7 @@
 SettingsDialog::SettingsDialog(int tab, QWidget *parent) : MasterDialog(parent),
         ui(new Ui::SettingsDialog) {
     ui->setupUi(this);
-
-//    QRect rect = geometry();
-//    if (rect.height() > 700) {
-//        rect.setHeight(700);
-//        setGeometry(rect);
-//    }
+    resize(1, 1);
 
     ui->connectionTestLabel->hide();
     ui->connectButton->setDefault(true);
