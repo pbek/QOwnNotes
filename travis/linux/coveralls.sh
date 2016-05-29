425111ac71baf5b17811a29a0ca6aba61d03e19a
Patrizio Bekerle <office@bekerle.com>
2016-05-29 09:16:40 +0200

    using an extenal coveralls.sh script to prevent random travis errors

---
+++ travis/linux/coveralls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# does the coveralls submit under linux
+#
+
+sudo pip install cpp-coveralls
+coveralls -b `pwd`/unit_tests/ -r `readlink -m ./../` -e tests/ -E .+/moc_.* -E .+/qrc_.* -E .+/qxt.* -E .+/ui_.*  > coveralls.log
+exit 0
