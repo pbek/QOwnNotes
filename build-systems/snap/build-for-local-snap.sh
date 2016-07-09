689aaa840f95a5c6a54ac9996d82109050d93adb
Patrizio Bekerle <office@bekerle.com>
2016-07-09 10:35:47 +0200

    added a local build script for the snap image

---
+++ build-systems/snap/build-for-local-snap.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#
+# This is the build script for a local qownnotes snap image and upload to the
+# store
+# https://myapps.developer.ubuntu.com/dev/click-apps/
+#
+# We need to be logged in to ubuntu one with:
+# snapcraft login
+#
+# The QOwnNotes repository for Launchpad Snap relies on our source archive on
+# https://sourceforge.net/projects/qownnotes/files/src/
+#
+
+
+# uncomment this if you want to force a version
+#QOWNNOTES_VERSION=16.07.3
+
+BRANCH=develop
+#BRANCH=master
+
+PROJECT_PATH="/tmp/QOwnNotes-launchpad-snap-$$"
+CUR_DIR=$(pwd)
+
+
+echo "Started the Snap building process, using latest '$BRANCH' git tree"
+
+if [ -d $PROJECT_PATH ]; then
+    rm -rf $PROJECT_PATH
+fi
+
+mkdir $PROJECT_PATH
+cd $PROJECT_PATH
+
+echo "Project path: $PROJECT_PATH"
+
+# checkout the source code
+git clone --depth=5 git@github.com:pbek/QOwnNotes.git QOwnNotes -b $BRANCH
+cd QOwnNotes
+
+if [ -z $QOWNNOTES_VERSION ]; then
+    # get version from version.h
+    QOWNNOTES_VERSION=`cat src/version.h | sed "s/[^0-9,.]//g"`
+fi
+
+cd build-systems/snap/snapcraft
+
+# replace the version in the snapcraft.yaml file
+sed -i "s/VERSION-STRING/$QOWNNOTES_VERSION/g" snapcraft.yaml
+
+snapcraft update
+
+echo "Building snap..."
+snapcraft
+
+echo "Uploading snap..."
+snapcraft upload qownnotes_${QOWNNOTES_VERSION}_amd64.snap
+
+# remove everything after we are done
+if [ -d $PROJECT_PATH ]; then
+    rm -rf $PROJECT_PATH
+fi
