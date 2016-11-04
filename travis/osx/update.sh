7c33a83c5701732550447a342b7d0979710d9a37
Patrizio Bekerle <patrizio@bekerle.com>
2016-11-04 05:40:24 +0100

    more macOS updater script implementation

--- travis/osx/update.sh
+++ travis/osx/update.sh
@@ -5,16 +5,26 @@
 #
 ################################################
 
-# todo: should be parameter
 #diskImageUrl="https://github.com/pbek/QOwnNotes/releases/download/macosx-b2381-103543/QOwnNotes-16.10.10.dmg"
-diskImageUrl="https://github.com/pbek/QOwnNotes/releases/download/macosx-b2376-114124/QOwnNotes-16.10.9.dmg"
+#diskImageUrl="https://github.com/pbek/QOwnNotes/releases/download/macosx-b2376-114124/QOwnNotes-16.10.9.dmg"
+diskImageUrl="$1"
 
-# todo: should be parameter
-applicationsFolder="/Applications"
+#applicationsFolder="/Applications"
+applicationsFolder="$2"
 
 # https://bash.cyberciti.biz/guide/$IFS
 IFS="
 "
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+echo "QOwnNotes macOS updater script"
+echo
+
+# check if we have 2 parameters
+[ "$#" -eq 2 ] || die "2 arguments required, $# provided"
 
 tempDir=`mktemp -d` && cd $tempDir
 mountPoint=`mktemp -d`
