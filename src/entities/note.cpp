935ddf0085f97a7225ec7b295b15ebbeb3dd12ca
Patrizio Bekerle <patrizio@bekerle.com>
2016-12-25 09:22:37 +0100

    added a comment

--- src/entities/note.cpp
+++ src/entities/note.cpp
@@ -1296,6 +1296,7 @@ QString Note::toMarkdownHtml(QString notesPath, int maxImageWidth,
     // try to replace file links like <my-note.md> to note links
     // this is a "has not '\w+:\/\/' in it" regular expression
     // see: http://stackoverflow.com/questions/406230/regular-expression-to-match-line-that-doesnt-contain-a-word
+    // TODO: maybe we could do that per QTextBlock to check if it's done in comment block?
     QRegularExpressionMatchIterator i =
             QRegularExpression("<(((?!\\w+:\\/\\/)[^<>])+)>").globalMatch(str);
     while (i.hasNext()) {
