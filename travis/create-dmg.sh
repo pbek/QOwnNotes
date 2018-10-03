ea90db58306f18067c64e9679e60547375e1f750
Patrizio Bekerle <patrizio@bekerle.com>
2018-10-03 06:45:48 +0200

    fixed version number adding

--- travis/create-dmg.sh
+++ travis/create-dmg.sh
@@ -21,8 +21,8 @@ echo "Changing bundle identifier"
 sed -i -e 's/com.yourcompany.QOwnNotes/com.PBE.QOwnNotes/g' $APP.app/Contents/Info.plist
 
 # adding version number
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $VERSION_NUMBER" $APP.app/Contents/Info.plist
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VERSION_NUMBER" $APP.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $VERSION_NUMBER" $APP.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string $VERSION_NUMBER" $APP.app/Contents/Info.plist
 
 # removing backup plist
 rm -f $APP.app/Contents/Info.plist-e
