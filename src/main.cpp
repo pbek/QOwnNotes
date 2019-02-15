3b25156a80de9c08b102f9a5f50facc9af07e3c8
Le Liu <liulex@users.noreply.github.com>
2019-02-16 02:15:34 +0800

    fixed settings lost after canceling the welcome dialog (#1150)

--- src/main.cpp
+++ src/main.cpp
@@ -204,7 +204,6 @@ bool mainStartupMisc(const QStringList &arguments) {
         WelcomeDialog welcomeDialog;
         // exit QOwnNotes if the welcome dialog was canceled
         if (welcomeDialog.exec() != QDialog::Accepted) {
-            settings.clear();
             return false;
         }
     }
