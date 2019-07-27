eb2a3cc2f4ef8da65b903b20cd93e4e7afdd1d9b
Patrizio Bekerle <patrizio@bekerle.com>
2019-07-27 23:06:20 +0200

    Remove cosmic because it was marked obsolete

--- ubuntu-launchpad/build-for-launchpad.sh
+++ ubuntu-launchpad/build-for-launchpad.sh
@@ -15,7 +15,7 @@
 
 BRANCH=develop
 #BRANCH=master
-UBUNTU_RELEASES=( "xenial" "bionic" "cosmic" "disco" "eoan" )
+UBUNTU_RELEASES=( "xenial" "bionic" "disco" "eoan" )
 
 
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
