08478af1ed763d426c79d9db2a7018c5c735dde2
Waqar Ahmed <waqar.17a@gmail.com>
2019-12-03 14:48:19 +0500

    Allow syntax highlighting

--- src/helpers/qownnotesmarkdownhighlighter.cpp
+++ src/helpers/qownnotesmarkdownhighlighter.cpp
@@ -134,6 +134,7 @@ void QOwnNotesMarkdownHighlighter::highlightMarkdown(const QString& text) {
     if (codeHighlightingOn) {
         if (previousBlockState() == HighlighterState::CodeBlock ||
             previousBlockState() == HighlighterState::CodeBlockEnd ||
+            previousBlockState() >= 200 ||
             text.contains(QStringLiteral("```"))) {
             highlightCodeBlock(text);
         }
