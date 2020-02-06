0e204c98c320a765657df8056c85b64d8130b25c
Waqar Ahmed <waqar.17a@gmail.com>
2020-02-06 15:38:02 +0500

    Clean up CodeToHtmlConverter

--- src/helpers/codetohtmlconverter.h
+++ src/helpers/codetohtmlconverter.h
@@ -50,8 +50,8 @@ public:
     QString process() const;
 
 private:
-    QStringRef _input;
-    QString _lang;
+    const QStringRef _input;
+    const QString _lang;
     Lang currentLang;
 
     QString escape(QChar c) const;
@@ -73,14 +73,14 @@ private:
     /**
      * @brief returns true if c is octal
      */
-    inline bool isOctal(const char c) const {
+    static inline bool isOctal(const char c) {
         return (c >= '0' && c <= '7');
     }
 
     /**
      * @brief returns true if c is hex
      */
-    inline bool isHex(const char c) const {
+    static inline bool isHex(const char c) {
         return (
             (c >= '0' && c <= '9') ||
             (c >= 'a' && c <= 'f') ||
