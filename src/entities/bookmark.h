439e86a7f99060236efd72e772c5eb7748db4ccf
Waqar Ahmed <waqar.17a@gmail.com>
2020-02-08 17:02:30 +0500

    Use cxx-11 range for instead of Q_FOREACH

--- src/entities/bookmark.h
+++ src/entities/bookmark.h
@@ -13,17 +13,17 @@
 
 class Bookmark {
 public:
-    explicit Bookmark();
+    Bookmark();
     explicit Bookmark(QString url, QString name = QString(),
             QStringList tagList = QStringList(), QString description = QString());
     friend QDebug operator<<(QDebug dbg, const Bookmark &bookmark);
-    QJsonObject jsonObject();
+    QJsonObject jsonObject() const;
     static QList<Bookmark> parseBookmarks(const QString& text, bool withBasicUrls = false);
-    static QString bookmarksWebServiceJsonText(QList<Bookmark> bookmarks);
+    static QString bookmarksWebServiceJsonText(const QList<Bookmark>& bookmarks);
     static QString parsedBookmarksWebServiceJsonText(const QString& text, bool withBasicUrls = false);
     bool operator==(const Bookmark &bookmark) const;
     static void mergeInList(QList<Bookmark> &bookmarks, Bookmark &bookmark);
-    static void mergeListInList(QList<Bookmark> &sourceBookmarks,
+    static void mergeListInList(const QList<Bookmark> &sourceBookmarks,
             QList<Bookmark> &destinationBookmarks);
     void mergeInList(QList<Bookmark> &bookmarks);
     void merge(Bookmark &bookmark);
