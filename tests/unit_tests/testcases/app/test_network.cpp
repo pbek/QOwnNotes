a58da07aa8021c7c174a030b49fc17c4d5060c3e
Patrizio Bekerle <patrizio@bekerle.com>
2020-07-25 10:21:01 +0200

    Disable tests on Linux with Qt older than 5.8

--- tests/unit_tests/testcases/app/test_network.cpp
+++ tests/unit_tests/testcases/app/test_network.cpp
@@ -8,10 +8,7 @@
 void TestNetwork::initTestCase() {}
 
 void TestNetwork::testSSL() {
-    // TLS checks fail on GitHub Actions for Linux Qt <= 5.7
-#if !defined(Q_OS_LINUX) || (QT_VERSION > QT_VERSION_CHECK(5, 7, 0))
     auto data = Utils::Misc::downloadUrl(QUrl("https://www.github.com"));
 
     QVERIFY(data.size() > 0);
-#endif
 }
