0f78d3af10d3dbdf775bd3448a53ffb9c40203c5
rootraven <6073574+r00tr4v3n@users.noreply.github.com>
2020-08-01 17:33:31 +0200

    cache folder api

--- src/services/scriptingservice.cpp
+++ src/services/scriptingservice.cpp
@@ -20,6 +20,7 @@
 #include <QQmlEngine>
 #include <QRegularExpression>
 #include <QSettings>
+#include <QStandardPaths>
 #include <QStringBuilder>
 #include <QTimer>
 #include <QVariant>
@@ -2218,4 +2219,37 @@ void ScriptingService::onScriptThreadDone(ScriptThread *thread) {
                 Q_ARG(QVariant, QVariant::fromValue(threadList)));
         }
     }
+}
+
+/**
+ * Returns the default cache folder of QON
+ * @param {QString} subFolder the subfolder to create and use
+ */
+QString ScriptingService::cacheFolder(const QString &subFolder) const {
+    QString cacheFolder = QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + QString("/scripts/");
+    if(!subFolder.isEmpty()){
+        cacheFolder = QDir::toNativeSeparators(cacheFolder  + subFolder);
+    }
+    QDir dir = QDir(cacheFolder);
+    if(!dir.exists()) {
+        dir.mkpath(dir.path());
+    }
+    return dir.path();
+}
+
+/**
+ * Clears the provided cache folder
+ * @param {QString} subFolder the subfolder to clear or nothing for the entire script cache folder
+ * @return {bool} true on success
+ */
+bool ScriptingService::clearCacheFolder(const QString &subFolder) const {
+    QString cacheFolder = QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + QString("/scripts/");
+    if(!subFolder.isEmpty()){
+        cacheFolder = QDir::toNativeSeparators(cacheFolder + subFolder);
+    }
+    QDir dir = QDir(cacheFolder);
+    if(!dir.exists()) {
+        return dir.rmdir(cacheFolder);
+    }
+    return false;
 }
