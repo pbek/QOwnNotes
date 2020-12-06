9033e2a0f504279c995f802d91d290c9be7afe72
Patrizio Bekerle <patrizio@bekerle.com>
2020-12-06 22:11:23 +0100

    Copy missing files (#1976)

--- build-systems/github/windows/windows-build-zip.ps1
+++ build-systems/github/windows/windows-build-zip.ps1
@@ -27,13 +27,13 @@ cd ..\release
 # Bug in Qt 5.14+: https://stackoverflow.com/questions/61045959/windeployqt-error-unable-to-find-the-platform-plugin
 windeployqt QOwnNotes.exe
 # this dlls where missed by windeployqt
-#copy ..\..\Qt\5.13.2\mingw73_64\bin\libwinpthread-1.dll .
-#copy ..\..\Qt\5.13.2\mingw73_64\bin\libgcc_s_seh-1.dll .
+copy ..\..\Qt\5.15.2\mingw81_64\bin\libwinpthread-1.dll .
+copy ..\..\Qt\5.15.2\mingw81_64\bin\libgcc_s_seh-1.dll .
 # this dll didn't work when released by windeployqt
 # important: this dll needs to be updated when a new version of Qt is used!
 # search for it in the mingw* folder of your local installation of Qt
 # Update: we are trying a direct copy again
-#copy ..\..\Qt\5.13.2\mingw73_64\bin\libstdc++-6.dll .
+copy ..\..\Qt\5.15.2\mingw81_64\bin\libstdc++-6.dll .
 # for some reason AppVeyor or windeployqt uses a copy of the German
 # translation file as English one, which screws up the English user interface
 #del translations\qt_en.qm
