29b54d8504ac02b72ee49e872beda2f15fc0c0ac
Patrizio Bekerle <patrizio@bekerle.com>
2020-12-06 21:11:40 +0100

    Try building windows release with Qt 5.15.2 (#1976)

--- build-systems/github/windows/windows-build-zip.ps1
+++ build-systems/github/windows/windows-build-zip.ps1
@@ -25,16 +25,16 @@ cd ..\release
 # fetching dependencies of QT app
 # http://doc.qt.io/qt-5/windows-deployment.html
 # Bug in Qt 5.14+: https://stackoverflow.com/questions/61045959/windeployqt-error-unable-to-find-the-platform-plugin
-windeployqt --release QOwnNotes.exe
+windeployqt QOwnNotes.exe
 # this dlls where missed by windeployqt
-copy ..\..\Qt\5.13.2\mingw73_64\bin\libwinpthread-1.dll .
-copy ..\..\Qt\5.13.2\mingw73_64\bin\libgcc_s_seh-1.dll .
+#copy ..\..\Qt\5.13.2\mingw73_64\bin\libwinpthread-1.dll .
+#copy ..\..\Qt\5.13.2\mingw73_64\bin\libgcc_s_seh-1.dll .
 # this dll didn't work when released by windeployqt
 # important: this dll needs to be updated when a new version of Qt is used!
 # search for it in the mingw* folder of your local installation of Qt
 # Update: we are trying a direct copy again
-copy ..\..\Qt\5.13.2\mingw73_64\bin\libstdc++-6.dll .
+#copy ..\..\Qt\5.13.2\mingw73_64\bin\libstdc++-6.dll .
 # for some reason AppVeyor or windeployqt uses a copy of the German
 # translation file as English one, which screws up the English user interface
-del translations\qt_en.qm
+#del translations\qt_en.qm
 Compress-Archive -Path * -DestinationPath ..\QOwnNotes.zip
