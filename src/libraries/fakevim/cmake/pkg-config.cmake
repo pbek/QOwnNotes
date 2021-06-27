fe4f16448527deb39c1985c1205d2752cb4aec12
Patrizio Bekerle <patrizio@bekerle.com>
2021-06-27 09:57:57 +0200

    Fix build process for cmake (#2134)

--- src/libraries/fakevim/cmake/pkg-config.cmake
+++ src/libraries/fakevim/cmake/pkg-config.cmake
@@ -4,10 +4,10 @@ set(PKG_CONFIG_INCLUDEDIR "\${prefix}/include/${bin}")
 set(PKG_CONFIG_LIBS "-L\${libdir} -l${bin}")
 set(PKG_CONFIG_CFLAGS "-I\${includedir}")
 
-configure_file(
-    "${CMAKE_CURRENT_SOURCE_DIR}/pkg-config.pc.in"
-    "${CMAKE_CURRENT_BINARY_DIR}/${bin}.pc"
-    )
+#configure_file(
+#    "${CMAKE_CURRENT_SOURCE_DIR}/pkg-config.pc.in"
+#    "${CMAKE_CURRENT_BINARY_DIR}/${bin}.pc"
+#    )
 
 set(INSTALL_PKG_CONFIG_DIR "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig"
     CACHE PATH "Installation directory for pkg-config files")
