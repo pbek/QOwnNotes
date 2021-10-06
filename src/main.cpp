c3a8ff80f4154408084f6adcbda2f8b09799f97f
Patrizio Bekerle <patrizio@bekerle.com>
2021-10-06 19:41:22 +0200

    Add missing parameters for loadMacTranslations(#783)

--- src/main.cpp
+++ src/main.cpp
@@ -648,7 +648,7 @@ int main(int argc, char *argv[]) {
         loadTranslations(translators, locale);
 
 #ifdef Q_OS_MAC
-        loadMacTranslations(translatorOSX, translatorOSX2,
+        loadMacTranslations(translatorOSX, translatorOSX2, translatorOSX3, translatorOSX4,
                             QCoreApplication::applicationDirPath(), locale);
 #endif
 
