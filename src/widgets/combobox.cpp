50fd5bdcb42d2375c0a767cd9702293a0fdde905
Patrizio Bekerle <patrizio@bekerle.com>
2021-12-29 11:51:00 +0100

    Update year to 2022

--- src/widgets/combobox.cpp
+++ src/widgets/combobox.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2021 Patrizio Bekerle -- <patrizio@bekerle.com>
+ * Copyright (c) 2014-2022 Patrizio Bekerle -- <patrizio@bekerle.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
