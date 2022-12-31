1ec8d2e9687d733301881b3c69ba2cd2e2eadcd3
Patrizio Bekerle <patrizio@bekerle.com>
2022-12-31 14:47:00 +0100

    Add script to update years

---
+++ scripts/update-copyright-year.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Use this script to update all copyright years in the source and license files
+#
+
+function help() {
+  echo "Usage: $0 <previous-year> <new-year>"
+  echo "Example: $0 2022 2023"
+  exit 1
+}
+
+if [ -z "$1" ]; then
+  help
+fi
+
+if [ -z "$2" ]; then
+  help
+fi
+
+echo "Previous year: $1"
+echo "New year: $2"
+
+
+echo
+echo Updating QOwnNotes...
+echo
+git checkout develop || exit 2
+git pull
+
+echo
+echo Updating piwiktracker...
+echo
+pushd src/libraries/piwiktracker || exit 1
+git checkout master || exit 2
+git pull
+popd || exit 3
+
+echo
+echo Updating qmarkdowntextedit...
+echo
+pushd src/libraries/qmarkdowntextedit || exit 1
+git checkout develop || exit 2
+git pull
+popd || exit 3
+
+echo
+echo Updating year "$1" to "$2"...
+echo
+sed -E "s/$1 Patrizio/$2 Patrizio/" -i $(find src -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name 'copyright' -o -name 'LICENSE')
+
+echo
+echo "Are there any $1 entries left? (should return nothing)"
+echo
+grep -r "$1 Patrizio" .
+
+echo
+echo "Commit message: \"Update year from $1 to $2\""
+echo
