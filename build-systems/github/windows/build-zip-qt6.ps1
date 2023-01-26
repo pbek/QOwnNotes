e36691a3e3f8f6c55be00d5b509731c1ee6cfe8b
Patrizio Bekerle <patrizio@bekerle.com>
2023-01-26 16:43:15 +0100

    #1304: Try new settings for macOS/Windows Qt6 release build job

---
+++ build-systems/github/windows/build-zip-qt6.ps1
@@ -0,0 +1,41 @@
+##################################################################################
+# This script builds QOwnNotes and creates a ZIP including all necessary libraries
+##################################################################################
+
+#dir -s ..\..\Qt
+Write-Host $Env:QT_VERSION
+echo "#define RELEASE ""GitHub Actions""" > release.h
+$Env:QMAKE $Env:QMAKE_PROJECT -r
+lrelease $Env:QMAKE_PROJECT
+make
+md ..\release
+# copy the binary to our release path
+copy release\QOwnNotes.exe ..\release
+# copy Win64 OpenSSL v1.1.1g DLLs to the release path
+copy ..\build-systems\github\windows\libcrypto-1_1-x64.dll ..\release
+copy ..\build-systems\github\windows\libssl-1_1-x64.dll ..\release
+# copy unzip application for updater
+copy ..\appveyor\unzip.exe ..\release
+# copy updater script
+copy ..\appveyor\update.bat ..\release
+# copy portable mode launcher to the release path
+copy ..\appveyor\QOwnNotesPortable.bat ..\release
+# copy translation files
+copy languages\*.qm ..\release
+cd ..\release
+# fetching dependencies of QT app
+# http://doc.qt.io/qt-5/windows-deployment.html
+# Bug in Qt 5.14+: https://stackoverflow.com/questions/61045959/windeployqt-error-unable-to-find-the-platform-plugin
+# Don't use "--release"! (maybe because of debug log?)
+windeployqt --debug QOwnNotes.exe
+# these dlls where missed by windeployqt
+copy ..\..\Qt\$Env:QT_VERSION\mingw81_64\bin\libwinpthread-1.dll .
+copy ..\..\Qt\$Env:QT_VERSION\mingw81_64\bin\libgcc_s_seh-1.dll .
+# this dll didn't work when released by windeployqt
+# important: this dll needs to be updated when a new version of Qt is used!
+# search for it in the mingw* folder of your local installation of Qt
+# Update: we are trying a direct copy again
+copy ..\..\Qt\$Env:QT_VERSION\mingw81_64\bin\libstdc++-6.dll .
+# create zip archive
+Compress-Archive -Path * -DestinationPath ..\QOwnNotes.zip
+$(CertUtil -hashfile ..\QOwnNotes.zip SHA256)[1] -replace " ","" | Out-File -FilePath ..\QOwnNotes.zip.sha256
