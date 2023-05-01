5b34581f6b9f2b04128f1f6a54ed277783143314
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-01 11:26:27 +0200

    fix: path

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -6,7 +6,7 @@
 pushd src || exit 1
 
 echo "Update translation files..."
-/usr/bin/lupdate -verbose QOwnNotes.pro
+lupdate -verbose QOwnNotes.pro
 
 #echo "Remove leading spaces in translation files..."
 #find ./languages/*.ts -type f ! -name QOwnNotes_en.ts -exec sed -i 's/^[[:space:]]*//' {} \;
