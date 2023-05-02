fc5262a3d1396fa38e56545c59ba79b105e80536
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-02 21:05:17 +0200

    dev: use direnv

--- shell.nix
+++ shell.nix
@@ -2,7 +2,12 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
+      pkgs.crowdin-cli
+      pkgs.buildPackages.cmakeWithGui
+      pkgs.buildPackages.qt5.qtbase
       pkgs.buildPackages.qt5.qmake
+      pkgs.buildPackages.qt5.qttools
+      pkgs.buildPackages.qt5.wrapQtAppsHook
       pkgs.buildPackages.qt5.qtx11extras
       pkgs.buildPackages.qt5.qtwebsockets
       pkgs.buildPackages.qt5.qtdeclarative
