5371a9c59518b5fa486766481852c284da850ce2
Patrizio Bekerle <patrizio@bekerle.com>
2024-01-07 19:27:03 +0100

    #2876 snap: try to install to the correct path

--- snap/local/wrapper.sh
+++ snap/local/wrapper.sh
@@ -2,5 +2,5 @@
 
 pwd
 find $SNAP
-# desktop-launch $SNAP/usr/bin/QOwnNotes -style=Breeze --snap "$@"
-desktop-launch $SNAP/QOwnNotes -style=Breeze --snap "$@"
+desktop-launch $SNAP/usr/bin/QOwnNotes -style=Breeze --snap "$@"
+# desktop-launch $SNAP/QOwnNotes -style=Breeze --snap "$@"
