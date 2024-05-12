4a48c31a5508d07c6e5b839787214916659d0110
Patrizio Bekerle <patrizio@bekerle.com>
2024-05-12 08:40:59 +0200

    dev: use nix path

--- src/icons/breeze-dark-qownnotes/copy-from-breeze-dark.sh
+++ src/icons/breeze-dark-qownnotes/copy-from-breeze-dark.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-breezeDarkIconPath="/usr/share/icons/breeze-dark/actions/16/"
+#breezeDarkIconPath="/usr/share/icons/breeze-dark/actions/16/"
+breezeDarkIconPath="/run/current-system/sw/share/icons/breeze-dark/actions/16/"
 
-cd ../breeze-qownnotes/16x16
+cd ../breeze-qownnotes/16x16 || exit
 find *.svg | xargs -I %% cp ${breezeDarkIconPath}%% ../../breeze-dark-qownnotes/16x16
