c8e74837cdbfd1cb76f907770942b68d22b38995
Patrizio Bekerle <patrizio@bekerle.com>
2024-08-06 22:23:51 +0200

    #3076 nix: add QT_TRANSLATIONS_DIR
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- default.nix
+++ default.nix
@@ -48,6 +48,7 @@ stdenv.mkDerivation {
     "-DQON_QT6_BUILD=ON"
     "-DUSE_SYSTEM_BOTAN=1"
     "-DBUILD_WITH_SYSTEM_BOTAN=ON"
+    "-DQT_TRANSLATIONS_DIR=./translations"
   ];
 
   postInstall = ''
