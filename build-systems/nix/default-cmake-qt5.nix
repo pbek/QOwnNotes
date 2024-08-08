c60f66058b5e23c7d77666c2927b665c0fe1e148
Patrizio Bekerle <patrizio@bekerle.com>
2024-08-08 19:44:48 +0200

    #3076 nix: fix build path
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- build-systems/nix/default-cmake-qt5.nix
+++ build-systems/nix/default-cmake-qt5.nix
@@ -26,7 +26,7 @@ in
 stdenv.mkDerivation {
   inherit pname appname version;
 
-  src = builtins.path { path = ./src; name = "qownnotes"; };
+  src = builtins.path { path = ../../src; name = "qownnotes"; };
 
   nativeBuildInputs = [
     cmake
