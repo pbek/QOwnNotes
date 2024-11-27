517329457f1516ee2ba5d2e564643adf258ed5f9
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-27 18:45:27 +0100

    webpage: update links
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- webpage/src/.vuepress/theme/layouts/Layout.vue
+++ webpage/src/.vuepress/theme/layouts/Layout.vue
@@ -3,12 +3,11 @@
     <template #page-bottom>
       <div class="page-nav">
         <div>
-          Made by <a href="https://www.keybase.io/pbek" target="_blank">Patrizio Bekerle</a> with ❤️
+          Crafted by <a href="https://bekerle.com" target="_blank">Patrizio Bekerle</a> with ❤️
         </div>
         <div class="social">
           <a href="https://github.com/pbek/QOwnNotes" target="_blank"><v-icon>mdi-github</v-icon></a>
-          <a href="https://twitter.com/QOwnNotes" target="_blank"><v-icon>mdi-twitter</v-icon></a>
-          <a href="https://www.facebook.com/QOwnNotes" target="_blank"><v-icon>mdi-facebook</v-icon></a>
+          <a href="https://social.qownnotes.org/@qownnotes" target="_blank"><v-icon>mdi-mastodon</v-icon></a>
         </div>
       </div>
 
