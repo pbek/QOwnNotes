0e4e6bacf153981b850b8160981d130c7b5e313a
Patrizio Bekerle <patrizio@bekerle.com>
2024-12-20 07:25:56 +0100

    doc: update year from 2024 to 2025
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- src/utils/schema.cpp
+++ src/utils/schema.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2024 Patrizio Bekerle -- <patrizio@bekerle.com>
+ * Copyright (c) 2014-2025 Patrizio Bekerle -- <patrizio@bekerle.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
