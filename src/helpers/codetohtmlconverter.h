a4cfd1bd0afb150eae5cba9c4a8daaa7a1fff21f
DarienMC <darienmusecharbonneau@gmail.com>
2025-01-31 23:49:10 -0700

    Add GDScript Language Data for Markdown Highlighting.
    Pulled data from the following pages from the Godot documentation:
    [All Classes](https://docs.godotengine.org/en/stable/classes/index.html)
    [@GDScript](https://docs.godotengine.org/en/stable/classes/class_%40gdscript.html)
    [@GlobalScope](https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html)
    [GDScript reference](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html)

--- src/helpers/codetohtmlconverter.h
+++ src/helpers/codetohtmlconverter.h
@@ -51,7 +51,8 @@ class CodeToHtmlConverter {
         CodeMake,
         CodeNix,
         CodeForth,
-        CodeSystemVerilog
+        CodeSystemVerilog,
+        CodeGDScript
     };
 
    public:
