caa7ec49917c9582fcb3f57fa2f8aa9fd1453fc2
Weej1 <Weej1@users.noreply.github.com>
2025-02-12 20:29:27 +0700

    Update openaiservice.cpp
    Hi @pbek! 
    
    Groq has updated their list of AI models supported, I've added in 2 of them used known for their coding abilities, the others I dont quite know if they should be included because one has to do with images, and a few others whose functions I can't determine. If you still would like to include them let me know.

--- src/services/openaiservice.cpp
+++ src/services/openaiservice.cpp
@@ -79,7 +79,7 @@ void OpenAiService::initializeBackends() {
     _backendModels[QStringLiteral("openai")] =
         QStringList{"gpt-4o", "gpt-4o-mini", "gpt-4-turbo", "gpt-3.5-turbo", "gpt-4"};
     _backendModels[QStringLiteral("groq")] =
-        QStringList{"deepseek-r1-distill-llama-70b", "llama-3.1-8b-instant", "llama-3.2-11b-vision-preview", "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-90b-vision-preview", "llama-3.3-70b-specdec", "llama-3.3-70b-versatile", "llama3-70b-8192", "llama3-8b-8192", "llama-guard-3-8b", "mixtral-8x7b-32768",
+        QStringList{"qwen-2.5-32b", "deepseek-r1-distill-qwen-32b", "deepseek-r1-distill-llama-70b", "llama-3.1-8b-instant", "llama-3.2-11b-vision-preview", "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-90b-vision-preview", "llama-3.3-70b-specdec", "llama-3.3-70b-versatile", "llama3-70b-8192", "llama3-8b-8192", "llama-guard-3-8b", "mixtral-8x7b-32768",
                     "gemma2-9b-it"};
 
     _backendApiBaseUrls.clear();
