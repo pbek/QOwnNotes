cb1139685ce163ee9db565f82a5cb700d4f63d3e
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-22 11:55:18 +0100

    git: improve formatting workflow
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,11 +3,8 @@
 
 echo "Running pre-commit hook with treefmt"
 
-# Run treefmt to format the code
-just fmt --fail-on-change
-
-# Check if there are any changes after formatting
-#if ! git diff-index --quiet HEAD --; then
-#  echo "Code has been formatted. Please review the changes and commit again."
-#  exit 1
-#fi
+# Run treefmt to format the code and check if there are any changes after formatting
+if ! just fmt --fail-on-change; then
+  echo "Code has been formatted. Please review the changes and commit again."
+  exit 1
+fi
