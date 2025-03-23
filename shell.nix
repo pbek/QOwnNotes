c1265370dea13efbe5d058bdb99f442426d74a9b
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-23 21:53:03 +0100

    nix: make sure the links are created in the correct path
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -53,9 +53,15 @@ pkgs.mkShell {
     ];
 
   shellHook = ''
-    # Symlink the pre-commit hook into the .git/hooks directory
-    ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
+    # Determine the repository root
+    REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 
-    ./scripts/nix-update-qmake-symlinks.sh
+    # If we are in the repository root
+    if [ "$REPO_ROOT" = "$(pwd)" ]; then
+        # Symlink the pre-commit hook into the .git/hooks directory
+        ln -sf ./scripts/pre-commit.sh .git/hooks/pre-commit
+
+        ./scripts/nix-update-qmake-symlinks.sh
+    fi
   '';
 }
