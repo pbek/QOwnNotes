1951d8ad2a65f3a00af0137abc0d6f93b27f1a24
Patrizio Bekerle <patrizio@bekerle.com>
2025-05-04 19:50:50 +0200

    #3267 release: disable Ubuntu 25.10 qmake launchpad build process
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- ubuntu-launchpad/build-for-launchpad.sh
+++ ubuntu-launchpad/build-for-launchpad.sh
@@ -16,7 +16,9 @@ BRANCH=main
 #BRANCH=release
 
 # https://wiki.ubuntu.com/Releases
-UBUNTU_RELEASES=("bionic" "focal" "jammy" "noble" "oracular" "plucky" "questing")
+# Ubuntu 25.10 Questing doesn't support QMake anymore!
+# See https://github.com/pbek/QOwnNotes/issues/3267
+UBUNTU_RELEASES=("bionic" "focal" "jammy" "noble" "oracular" "plucky")
 
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
 PROJECT_PATH="/tmp/QOwnNotes-$$"
