66b4ebf4b5f3b35af001fd0fbfde4fa62c58ac9e
Patrizio Bekerle <patrizio@bekerle.com>
2025-07-13 19:33:04 +0200

    release: remove Ubuntu 24.10 PPA build process, because Ubuntu Launchpad dropped support
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- ubuntu-launchpad/build-for-launchpad-qt6.sh
+++ ubuntu-launchpad/build-for-launchpad-qt6.sh
@@ -19,7 +19,7 @@ BRANCH=main
 #BRANCH=release
 
 # https://wiki.ubuntu.com/Releases
-UBUNTU_RELEASES=("noble" "oracular" "plucky" "questing")
+UBUNTU_RELEASES=("noble" "plucky" "questing")
 
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
 PROJECT_PATH="/tmp/QOwnNotes-$$"
