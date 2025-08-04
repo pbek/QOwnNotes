5679394273d4513aee329ccb85a623f69a8fd0db
Patrizio Bekerle <patrizio@bekerle.com>
2025-08-04 20:14:29 +0200

    #3334 logwidget: ignore more warnings for Qt 6.9.1
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- src/widgets/logwidget.cpp
+++ src/widgets/logwidget.cpp
@@ -143,6 +143,11 @@ void LogWidget::log(LogWidget::LogType logType, const QString &text) {
          text.contains(QLatin1String("[Botan Error]  Invalid CBC padding")) ||
          text.contains(QLatin1String("Invalid version or not a cyphertext")) ||
          text.contains(QLatin1String("QTextCursor::setPosition: Position")) ||
+         text.contains(QLatin1String("which does not match the current topmost grabbing popup,")) ||
+         text.contains(QLatin1String(
+             "QObject::disconnect: wildcard call disconnects from destroyed signal of")) ||
+         text.contains(
+             QLatin1String("This plugin supports grabbing the mouse only for popup windows")) ||
          text.contains(
              QLatin1String("QFont::setPointSizeF: Point size <= 0")) ||    // we don't even use that
                                                                            // method directly
