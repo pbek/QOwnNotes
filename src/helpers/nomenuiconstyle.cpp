a990d3fbdc443905186589d28831404cd9ca6235
Patrizio Bekerle <patrizio@bekerle.com>
2026-03-16 17:35:57 +0100

    #3499 navigationwidget: fix Qt 5.9 build error
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- src/helpers/nomenuiconstyle.cpp
+++ src/helpers/nomenuiconstyle.cpp
@@ -4,9 +4,20 @@ void NoMenuIconStyle::drawControl(ControlElement element, const QStyleOption *op
                                   QPainter *painter, const QWidget *widget) const {
     if (element == CE_MenuItem && widget && qobject_cast<const QMenu *>(widget)) {
         if (const auto *menuItem = qstyleoption_cast<const QStyleOptionMenuItem *>(option)) {
+#ifdef __clang__
+#if __has_warning("-Wdeprecated-copy-with-user-provided-copy")
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-copy-with-user-provided-copy"
+#endif
+#endif
             QStyleOptionMenuItem opt(*menuItem);    // Copy original option
-            opt.icon = QIcon();                     // Remove the icon
-            opt.maxIconWidth = 0;                   // Prevent space allocation
+#ifdef __clang__
+#if __has_warning("-Wdeprecated-copy-with-user-provided-copy")
+#pragma clang diagnostic pop
+#endif
+#endif
+            opt.icon = QIcon();      // Remove the icon
+            opt.maxIconWidth = 0;    // Prevent space allocation
 
             QProxyStyle::drawControl(element, &opt, painter, widget);
             return;
