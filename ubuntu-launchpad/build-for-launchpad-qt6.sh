9ab7e598b5fa79aaeae7d0fc1f9ce1acf3f107fc
Patrizio Bekerle <patrizio@bekerle.com>
2026-07-23 21:10:53 +0200

    release: remove not anymore supported Ubuntu Questing build

--- ubuntu-launchpad/build-for-launchpad-qt6.sh
+++ ubuntu-launchpad/build-for-launchpad-qt6.sh
@@ -19,7 +19,7 @@ BRANCH=main
 #BRANCH=release
 
 # https://wiki.ubuntu.com/Releases
-UBUNTU_RELEASES=("noble" "questing" "resolute" "stonking")
+UBUNTU_RELEASES=("noble" "resolute" "stonking")
 
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
 PROJECT_PATH="/tmp/QOwnNotes-$$"
